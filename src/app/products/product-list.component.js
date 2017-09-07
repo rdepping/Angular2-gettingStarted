67637edd0bc5a33d671606ce645ece9da60b7cf9
Ralph Depping <github@depping.me>
2017-09-07 13:19:56 +0100

    Product list via service
    some inital http stuff in here as well....

--- src/app/products/product-list.component.js
+++ src/app/products/product-list.component.js
@@ -9,54 +9,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var product_service_1 = require("./product.service");
 var ProductListComponent = (function () {
-    function ProductListComponent() {
+    function ProductListComponent(_productService) {
+        this._productService = _productService;
         this.pageTitle = 'Product List';
         this.imageWidth = 50;
         this.imageMargin = 2;
         this.showImage = false;
-        this.products = [{
-                'productId': 1,
-                'productName': 'Leaf Rake',
-                'productCode': 'GDN-0011',
-                'releaseDate': 'March 19, 2016',
-                'description': 'Leaf rake with 48-inch wooden handle.',
-                'price': 19.95,
-                'starRating': 3.2,
-                'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-            },
-            {
-                'productId': 2,
-                'productName': 'Garden Cart',
-                'productCode': 'GDN-0023',
-                'releaseDate': 'March 18, 2016',
-                'description': '15 gallon capacity rolling garden cart',
-                'price': 32.99,
-                'starRating': 4.2,
-                'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-            },
-            {
-                'productId': 5,
-                'productName': 'Hammer',
-                'productCode': 'TBX-0048',
-                'releaseDate': 'May 21, 2016',
-                'description': 'Curved claw steel hammer',
-                'price': 8.9,
-                'starRating': 4.8,
-                'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
-            },
-            {
-                'productId': 8,
-                'productName': 'Saw',
-                'productCode': 'TBX-0022',
-                'releaseDate': 'May 15, 2016',
-                'description': '15-inch steel blade hand saw',
-                'price': 11.55,
-                'starRating': 3.7,
-                'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png'
-            }];
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+        this.products = [];
     }
     Object.defineProperty(ProductListComponent.prototype, "listFilter", {
         get: function () {
@@ -80,6 +41,8 @@ var ProductListComponent = (function () {
     };
     ProductListComponent.prototype.ngOnInit = function () {
         console.log('In OnInit');
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     };
     ProductListComponent.prototype.onRatingClicked = function (message) {
         this.pageTitle = 'Product List: ' + message;
@@ -92,7 +55,7 @@ ProductListComponent = __decorate([
         templateUrl: 'app/products/product-list-component.html',
         styleUrls: ['app/products/product-list.component.css']
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [product_service_1.ProductService])
 ], ProductListComponent);
 exports.ProductListComponent = ProductListComponent;
 //# sourceMappingURL=product-list.component.js.map
