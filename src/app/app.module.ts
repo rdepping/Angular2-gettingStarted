ed59a806796d5e88646448e978a2bb17fcf303ad
Ralph Depping <github@depping.me>
2017-09-07 15:24:27 +0100

    initial routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component';
 import { ConvertToSpacesPipe }  from './shared/convert-to-spaces';
 import { StarComponent }  from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, HttpModule ],
+  imports: [ BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([], {useHash: true}) ],
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent],
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
